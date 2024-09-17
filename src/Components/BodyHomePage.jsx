df14e0179653869fb13175d4fef5b088ade3d047
DomicMae <ardonyunors147@gmail.com>
2024-09-17 13:58:32 +0700

    Revision Design

--- src/Components/BodyHomePage.jsx
+++ src/Components/BodyHomePage.jsx
@@ -5,29 +5,27 @@ const BodyHomePage = ({ user }) => {
     <div className="text-black pb-20">
       {" "}
       <div className="grid grid-cols-2 gap-4 p-10 ">
-        <div className="col-span-1 flex">
-          <div className="grid grid-rows-1">
-            <div className="flex flex-col pl-5">
-              {/* Semua teks di dalam div ini akan berwarna hitam */}
-              <h1 className="text-4xl font-bold sm:text-5xl text-custom-orange pb-8">
-                Hello!!
-              </h1>
-              <h1 className="text-4xl font-bold sm:text-5xl text-white">
-                I’m Ardon Yunors Tallan,
-              </h1>
-              <h1 className="text-4xl font-bold sm:text-5xl text-custom-orange pb-8">
-                Fresh Graduated Programmer
-              </h1>
+        <div className="col-span-1 flex-col">
+          {/* <div className="flex flex-col pl-5"> */}
+          {/* Semua teks di dalam div ini akan berwarna hitam */}
+          <h1 className="text-4xl font-bold sm:text-5xl text-custom-orange pb-8">
+            Hello!!
+          </h1>
+          <h1 className="text-4xl font-bold sm:text-5xl text-white">
+            I’m Ardon Yunors Tallan,
+          </h1>
+          <h1 className="text-4xl font-bold sm:text-5xl text-custom-orange pb-8">
+            Fresh Graduated Programmer
+          </h1>
 
-              {/* Kalimat ("") */}
-              <p className="text-white text-xl font-light">
-                Kuatkan dan teguhkanlah hatimu, janganlah takut dan gemetar
-                karena mereka, karena Tuhan, Allahmu, Dialah yang berjalan
-                menyertai engkau; Ia tidak akan membiarkan engkau dan tidak akan
-                meninggalkan engkau.
-              </p>
-            </div>
-          </div>
+          {/* Kalimat ("") */}
+          <p className="text-white text-xl font-light">
+            Kuatkan dan teguhkanlah hatimu, janganlah takut dan gemetar karena
+            mereka, karena Tuhan, Allahmu, Dialah yang berjalan menyertai
+            engkau; Ia tidak akan membiarkan engkau dan tidak akan meninggalkan
+            engkau.
+          </p>
+          {/* </div> */}
         </div>
 
         <div className="col-span-1 flex justify-center">
