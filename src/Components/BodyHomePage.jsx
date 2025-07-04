28fc46e9f1c004e1bb6746e351a0e0734f91a300
DomicMae <ardonyunors147@gmail.com>
2025-07-04 08:40:26 +0700

    feat(homepage): improve content, redesign layout, and add animations

--- src/Components/BodyHomePage.jsx
+++ src/Components/BodyHomePage.jsx
@@ -1,38 +1,88 @@
 import "../input.css";
+import { motion } from "framer-motion";
+import { BackgroundBeamsWithCollision } from "./ui/EffectHome";
 
 const BodyHomePage = ({ user }) => {
   return (
-    <div className="flex items-center justify-center min-h-screen text-black">
-      {/* Responsive grid layout */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {/* Left section with text */}
-        <div className="md:col-span-2 flex flex-col justify-center">
-          <div className="flex flex-col justify-center min-h- px-10 pb-20">
-            <h1 className="text-4xl md:text-5xl font-jakarta font-bold sm:text-5xl text-custom-orange pb-2">
-              Hello!!
-            </h1>
-            <h1 className="text-4xl md:text-5xl font-jakarta font-bold sm:text-5xl text-custom-white pb-2">
-              I’m Ardon Yunors Tallan,
-            </h1>
-            <h1 className="text-4xl md:text-5xl font-jakarta font-bold sm:text-5xl text-custom-orange pb-2">
-              Fresh Graduated Programmer
-            </h1>
-            <h1 className="text-4xl md:text-5xl font-jakarta font-bold sm:text-5xl text-custom-white pb-4 md:pb-8">
-              Web Developer
-            </h1>
+    <BackgroundBeamsWithCollision>
+      <div className="text-black">
+        {/* Responsive grid layout */}
+        <div className="w-full min-h-screen grid grid-cols-1 md:grid-cols-2 gap-4 px-6 md:px-10">
+          {/* Left section with text */}
+          <div className="flex flex-col justify-start md:justify-center items-start md:items-start pt-40 md:pt-0">
+            <div className="flex flex-col justify-center text-center md:text-start pb-0 md:pb-20 w-full">
+              <motion.h1
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6 }}
+                className="text-3xl sm:text-4xl md:text-6xl font-jakarta font-bold text-custom-orange pb-2"
+              >
+                Hello!!
+              </motion.h1>
+              <motion.h1
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6 }}
+                className="block md:hidden text-3xl sm:text-4xl font-jakarta font-bold text-custom-white pb-0"
+              >
+                I’m
+              </motion.h1>
+              <motion.h1
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6 }}
+                className="block md:hidden text-3xl sm:text-4xl font-jakarta font-bold text-custom-white pb-2"
+              >
+                Ardon Yunors Tallan
+              </motion.h1>
+
+              {/* Teks untuk md ke atas: gabungan */}
+              <motion.h1
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6 }}
+                className="hidden md:block text-6xl font-jakarta font-bold text-custom-white pb-2"
+              >
+                I’m Ardon Yunors Tallan,
+              </motion.h1>
+
+              <motion.h1
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6 }}
+                className="text-3xl sm:text-4xl md:text-6xl font-jakarta font-bold text-custom-orange pb-2"
+              >
+                Fresh Graduated Programmer
+              </motion.h1>
+
+              <motion.h1
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6 }}
+                className="text-3xl sm:text-4xl md:text-6xl font-jakarta font-bold text-custom-white pb-4 md:pb-8"
+              >
+                Creative Software Developer
+              </motion.h1>
+            </div>
+          </div>
+
+          {/* Right section with image */}
+          <div className="hidden md:flex justify-center items-center pb-0 md:pb-20 relative">
+            <div
+              className="absolute -rotate-3 w-[24rem] h-[36rem] bg-gradient-to-br from-pink-500 via-indigo-500 to-purple-500
+                  blur-2xl opacity-20 rounded-xl z-0"
+            />
+
+            <img
+              src="FotoMe-Light.jpg"
+              alt="Foto Ardon"
+              className="w-[22rem] h-[32rem] object-cover rounded-3xl shadow-2xl
+               border-[1px] border-white/10 backdrop-blur-md z-10"
+            />
           </div>
         </div>
-
-        {/* Right section with image */}
-        <div className="md:col-span-1 flex justify-center items-center pb-20">
-          <img
-            src="foto.svg"
-            alt="Description"
-            className="rounded-full w-108 h-108 md:w-116 md:h-116"
-          />
-        </div>
       </div>
-    </div>
+    </BackgroundBeamsWithCollision>
   );
 };
 
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,10 +7,13 @@ import BodyAboutMePage from "../Components/BodyAboutMePage";
 import BodyProject from "../Components/BodyProject";
 import BodyContactPage from "../Components/BodyContactPage";
 import BodySertifikatPage from "../Components/BodySertifikatPage";
+import { motion, useScroll } from "framer-motion";
 
 export default function HomePage(props) {
   const [isLoading, setIsLoading] = useState(true);
   const [showContent, setShowContent] = useState(false);
+
+  const { scrollYProgress } = useScroll();
 
   useEffect(() => {
     // Disable right-click across the page
@@ -42,106 +45,123 @@ export default function HomePage(props) {
   }
 
   return (
-    <div
-      className={`min-h-screen flex flex-col bg-custom-red transition-opacity duration-1000 ${
-        showContent ? "opacity-100" : "opacity-0"
-      }`}
-    >
-      <Navbar user={props.auth.user} />
-      <main className="mt-24 font-jakarta">
-        <section id="body-home" className="w-full min-h-screen">
-          <BodyHomePage />
-        </section>
+    <>
+      {/* Scroll Indicator */}
+      <motion.div
+        style={{
+          scaleX: scrollYProgress,
+          position: "fixed",
+          bottom: 0, // 🔄 dari top ke bottom
+          left: 0,
+          right: 0,
+          height: 5,
+          originX: 0,
+          backgroundColor: "#DBAE52",
+          zIndex: 50,
+        }}
+      />
 
-        <section
-          id="about-me"
-          className="w-full min-h-screen py-10 bg-custom-black"
-        >
-          <BodyAboutMePage />
-        </section>
+      <div
+        className={`min-h-screen flex flex-col bg-custom-red transition-opacity duration-1000 ${
+          showContent ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <Navbar user={props.auth.user} />
+        <main className="mt-24 font-jakarta">
+          <section id="body-home" className="w-full min-h-screen">
+            <BodyHomePage />
+          </section>
 
-        <section id="skills" className="w-full min-h-screen py-10">
-          <BodySkill />
-        </section>
+          <section
+            id="about-me"
+            className="w-full min-h-screen py-10 bg-custom-black"
+          >
+            <BodyAboutMePage />
+          </section>
 
-        <section
-          id="project"
-          className="w-full min-h-screen py-10 bg-custom-black"
-        >
-          <BodyProject />
-        </section>
+          <section id="skills" className="w-full min-h-screen">
+            <BodySkill />
+          </section>
 
-        <section id="sertifikat" className="w-full min-h-screen py-10">
-          <BodySertifikatPage />
-        </section>
+          <section
+            id="project"
+            className="w-full min-h-screen py-10 bg-custom-black"
+          >
+            <BodyProject />
+          </section>
 
-        <section
-          id="contact"
-          className="w-full min-h-screen py-10 bg-custom-black"
-        >
-          <BodyContactPage />
-        </section>
+          <section id="sertifikat" className="w-full min-h-screen">
+            <BodySertifikatPage />
+          </section>
 
-        <footer className="footer bg-black text-white text-center pt-8 pb-8 flex flex-col md:flex-row justify-between px-6 md:px-12">
-          {/* aside 1 */}
-          <aside className="hidden md:flex flex-row items-start space-x-10">
-            <a href="/" className="flex ">
-              <img src="/favicon.png" alt="Logo" className="h-24" />
-            </a>
-            <div className="flex flex-col text-left">
-              <h2 className="text-lg font-semibold">YATA</h2>
-              <p className="text-sm max-w-md text-gray-400">
-                "Yatta" (やった) berarti "Aku berhasil!" atau "Yeay!", yang
-                sering digunakan untuk mengekspresikan kegembiraan atau
-                pencapaian.
-              </p>
-            </div>
-          </aside>
+          <section
+            id="contact"
+            className="w-full min-h-screen py-10 bg-custom-black"
+          >
+            <BodyContactPage />
+          </section>
 
-          {/* aside 2 */}
-          <aside className="flex flex-row items-center justify-center md:justify-end w-full md:w-auto space-x-6">
-            <div className="flex flex-col">
-              <p className="mb-5">
-                © {new Date().getFullYear()} Ardon - All rights reserved
-              </p>
+          <footer className="footer bg-black text-white text-center pt-8 pb-8 flex flex-col md:flex-row justify-between px-6 md:px-12">
+            {/* aside 1 */}
+            <aside className="hidden md:flex flex-row items-start space-x-10">
+              <a href="/" className="flex ">
+                <img src="/favicon.png" alt="Logo" className="h-24" />
+              </a>
+              <div className="flex flex-col text-left">
+                <h2 className="text-lg font-semibold">YATA</h2>
+                <p className="text-sm max-w-md text-gray-400">
+                  "Yatta" (やった) berarti "Aku berhasil!" atau "Yeay!", yang
+                  sering digunakan untuk mengekspresikan kegembiraan atau
+                  pencapaian.
+                </p>
+              </div>
+            </aside>
 
-              <nav className="">
-                <div className="flex justify-center gap-x-8">
-                  <a
-                    href="https://www.linkedin.com/in/ardonyunors/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <button>
-                      <Linkedin className="w-10 h-10" />
-                    </button>
-                  </a>
+            {/* aside 2 */}
+            <aside className="flex flex-row items-center justify-center md:justify-end w-full md:w-auto space-x-6">
+              <div className="flex flex-col">
+                <p className="mb-5">
+                  © {new Date().getFullYear()} Ardon - All rights reserved
+                </p>
 
-                  <a
-                    href="https://www.instagram.com/ardonyunors"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <button>
-                      <Instagram className="w-10 h-10" />
-                    </button>
-                  </a>
+                <nav className="">
+                  <div className="flex justify-center gap-x-8">
+                    <a
+                      href="https://www.linkedin.com/in/ardonyunors/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <button>
+                        <Linkedin className="w-10 h-10" />
+                      </button>
+                    </a>
 
-                  <a
-                    href="https://github.com/DomicMae"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <button>
-                      <Github className="w-10 h-10" />
-                    </button>
-                  </a>
-                </div>
-              </nav>
-            </div>
-          </aside>
-        </footer>
-      </main>
-    </div>
+                    <a
+                      href="https://www.instagram.com/ardonyunors"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <button>
+                        <Instagram className="w-10 h-10" />
+                      </button>
+                    </a>
+
+                    <a
+                      href="https://github.com/DomicMae"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <button>
+                        <Github className="w-10 h-10" />
+                      </button>
+                    </a>
+                  </div>
+                </nav>
+              </div>
+            </aside>
+          </footer>
+        </main>
+      </div>
+    </>
   );
 }
