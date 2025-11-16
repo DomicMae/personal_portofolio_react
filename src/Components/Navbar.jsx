2765ede1848b575757a66aa48e29652842501966
DomicMae <ardonyunors147@gmail.com>
2025-11-16 09:35:35 +0700

    style: update UI label from "resume" to "download CV"

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -164,7 +164,7 @@ const Navbar = () => {
                 onClick={handleDownload}
                 className="group relative text-black bg-white hover:bg-custom-blue hover:text-custom-white focus:outline-none focus:ring-0 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-white-600 dark:group-hover:bg-custom-blue transition-transform ease-in-out duration-300 transform hover:-translate-y-1 hover:scale-105"
               >
-                Resume
+                Download CV
               </button>
             </div>
           </div>
